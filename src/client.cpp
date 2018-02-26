3cc0a3a8835aaa58496b05737685f98f85926170
JoannahRavi <joni.ravi@gmail.com>
2018-02-26 16:57:28 -0500

    automate client

--- src/client.cpp
+++ src/client.cpp
@@ -162,29 +162,19 @@ void handle_file_transfer(string filename, string reply, char * data) {
     }
 }
 
-int main(int argc, char * argv[]) {
-    int sockfd, numbytes;
+
+int defaultmode(char *argv[], int port)
+{ 
+    int sockfd, numbytes;  
     char buffer[MAXLEN];
-    int port = 0;
+    //int port = 0;
 
-    // Command line arguments not provided
-    if (argc != 3) {
-        fprintf(stderr, "usage: client server_ip server_port\n");
-        exit(1);
-    }
-
-    // If entered port is invalid
-    if ((port = atoi(argv[2])) == 0 || port < 1024 || port > 65535) {
-        fprintf(stderr, "Please enter a valid port number\n");
-        exit(1);
-    }
 
     // Connect to server
-    if ((sockfd = connect_to_socket(port, argv[1])) == -1) {
+    if((sockfd = connect_to_socket(port, argv[1])) == -1){
         fprintf(stderr, "Error connecting.\n");
         exit(1);
     }
-
     // Saving server IP
     strcpy(server_ip, argv[1]);
 
@@ -194,34 +184,34 @@ int main(int argc, char * argv[]) {
     string token;
 
     // Main loop which takes user commands
-    while (1) {
+    while(1){
 
         // Clearing out buffer
         memset(buffer, 0, MAXLEN);
 
         // Receiving reply to command / prompt from server
-        if ((numbytes = recv(sockfd, buffer, MAXLEN - 1, 0)) == -1) {
+        if ((numbytes = recv(sockfd, buffer, MAXLEN-1, 0)) == -1){
             perror("recv");
             continue;
         }
         // recv returns zero if server closes connection
-        if (numbytes == 0) {
+        if (numbytes == 0){
             printf("Connection closed.\n");
             close(sockfd);
             exit(0);
         }
         buffer[numbytes] = '\0';
-        printf("%s", buffer);
+        printf("%s",buffer);
 
         // Check if reply was a get call
         istringstream iss(buffer);
         string reply;
-        if (iss >> reply && (reply == "get" || reply == "put")) {
+        if (iss >> reply && (reply == "get" || reply == "put")){
             handle_file_transfer(token, reply, buffer);
         }
 
         // Take user input
-        if (fgets(buffer, MAXLEN - 1, stdin) == NULL) {
+        if (fgets(buffer, MAXLEN-1, stdin) == NULL){
             fprintf(stderr, "Error reading command");
             close(sockfd);
             exit(1);
@@ -247,16 +237,150 @@ int main(int argc, char * argv[]) {
         }
 
         // Sending command to server
-        if (send(sockfd, buffer, strlen(buffer), 0) == -1) {
+        if (send(sockfd, buffer, strlen(buffer), 0) == -1){
             perror("send");
             continue;
-        }
-
-        if (token == "exit") {
+        }   
+        
+        if (token == "exit"){
             close(sockfd);
             printf("Connection closed.\n");
             break;
         }
     }
+    return 0;
+}
+
+//function that checks if a given filename exists or not
+
+
+void automatedmode(char *argv[], int port, FILE *fout)
+{   
+    int sockfd, numbytes;  
+    char buffer[MAXLEN];    
+    FILE * fin;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    if((fin = fopen(argv[3], "r"))== NULL){
+        perror("error opening file");
+        return;
+    }
+
+    //check from here 
+
+    if((sockfd = connect_to_socket(port, argv[1])) == -1){
+            fprintf(fout, "Error connecting.\n");
+            return;
+    }
+    strcpy(server_ip, argv[1]);
+
+    // Printing into out file
+    fprintf(fout,"Client: connecting to %s\n", argv[1]);    
+    
+    // To save filename of get/put
+    string token;
+
+    // Input from in file
+    while ((read = getline(&line, &len, fin)) != -1) {
+        
+        
+        // Clearing out buffer
+        memset(buffer, 0, MAXLEN);
+
+        // Receiving reply to command / prompt from server
+        if ((numbytes = recv(sockfd, buffer, MAXLEN-1, 0)) == -1){
+            perror("recv");
+            continue;
+        }
+        // recv returns zero if server closes connection
+        if (numbytes == 0){
+            fprintf(fout,"Connection closed.\n");
+            close(sockfd);
+            return;
+        }
+        buffer[numbytes] = '\0';
+
+        // Write to out file
+        fprintf(fout,"%s",buffer);
+
+        // Check if reply was a get call
+        istringstream iss(buffer);
+        string reply;
+        if (iss >> reply && (reply == "get" || reply == "put")){
+            handle_file_transfer(token, reply, buffer);
+        }
+
+        // Getting command from in file
+        strcpy(buffer,line);
+
+        // Saving filename in case of get / put
+        istringstream tokenize(buffer);
+        tokenize >> token;
+        if (token == "get" || token == "put") {
+            if (token == "put") {
+                // If its put, we need to make sure client has the file
+                struct stat file_stat;
+                tokenize >> token;
+                if (stat(token.c_str(), & file_stat) != 0) {
+                    // Invalidate put command
+                    strcpy(buffer, "put\n");
+                }
+            } else {
+                tokenize >> token;
+            }
+
+        }
+
+        // Sending command to server
+        if (send(sockfd, buffer, strlen(buffer), 0) == -1){
+            perror("send");
+            continue;
+        }   
+        
+        if (token == "exit"){
+            close(sockfd);
+            fprintf(fout,"Connection closed.\n");
+            break;
+        }
+        
+    }
+    
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    
+    int port = 0;
+
+    // Command line arguments not provided
+    //To accept default or automated mode
+    if (argc!=3 && argc!=5) {
+        fprintf(stderr,"usage: client server_ip server_port\n or \nusage: client server_ip server_port infile outfile\n");
+        exit(1);
+    }
+
+    // If entered port is invalid
+    if ((port = atoi(argv[2])) == 0 || port < 1024 || port > 65535){
+        fprintf(stderr, "Please enter a valid port number\n");
+        exit(1);
+    }
+
+    
+    if(argc==3)
+    {
+        defaultmode(argv, port);
+    }
+
+    if(argc==5)
+    {
+        FILE* fout = NULL;
+        fout = fopen(argv[4], "w");
+        automatedmode(argv, port, fout);
+        fclose(fout);
+    }
+    
     return 0;
 }
